a5877d1c248c19f5d0fb6644e13591a3d689b372
Abhi Das <abhidaskgpian@gmail.com>
2025-05-06 16:12:30 +0530

    Abhi Das

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, url_for, redirect,request, render_template, make_response
+from db import getTaskList, addTask
 app = Flask(__name__)
 
-tasklist = [['Walk Dog', True], ['Wash Dishes', False], ['Watch Movie', True]]
+# tasklist = [['Walk Dog', True], ['Wash Dishes', False], ['Watch Movie', True]]
+
 
 @app.route('/')
 def home():
+    tasklist = getTaskList()
     return render_template('tasklist.html', TaskList = tasklist)
+
+@app.route('/add', methods = ['POST'])
+def add():
+    taskname = request.form['taskName']
+    duedate = request.form['dueDate']
+    addTask(taskname, duedate)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__': 
     app.run(debug = True, port=5000)
